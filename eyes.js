ad66edc127ceb6c4200a314bfaf8a146ba1c33e8
AmandaMHorowitz <120349151+AmandaMHorowitz@users.noreply.github.com>
2023-03-19 08:44:00 -0400

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,19 @@
+const balls = document.getElementsByClassName('ball');
+
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  var myVar = 0;
+ 
+  for (let i = 0; i < 2; i++) {
+      if (i > 0) {
+        myVar = 20;
+      }
+      
+      balls[i].style.left = x;
+      balls[i].style.top = y;
+      balls[i].transform = 'translate(-' + x + myVar + ',-' + y + myVar + ')';
+    }
+
+};
